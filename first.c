757da3c670e598edc9ceb00749c3d89bee5031f2
babokim3 <babokim3@gmail.com>
2015-04-29 22:29:21 -0400

    second

--- first.c
+++ first.c
@@ -1,5 +1,8 @@
-#include<stdio.h>
 
+#include<stdio.h>
 void main(){
-	printf("hello jun. \n");
+	int i;
+	for(i = 0; i <5; i++)
+		printf("hello jun.\n");
 }
+
---
+++ fisrt1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main(){
+	int i;
+	for(i = 0; i < 5; i++){
+		printf("hello jun\n");
+}
